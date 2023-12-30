1bd755ebf444d363c187c9b4db6f0b907c8f2513
vascabarkapa <vascabarkapa@gmail.com>
2023-12-30 19:22:07 +0100

    auth footer

--- client/src/components/cards/AuthFooter.js
+++ client/src/components/cards/AuthFooter.js
@@ -1,4 +1,3 @@
-// material-ui
 import { useMediaQuery, Container, Link, Typography, Stack } from '@mui/material';
 
 // ==============================|| FOOTER - AUTHENTICATION ||============================== //
@@ -15,10 +14,9 @@ const AuthFooter = () => {
         textAlign={matchDownSM ? 'center' : 'inherit'}
       >
         <Typography variant="subtitle2" color="secondary" component="span">
-          &copy; Cloudberry React Dashboard Template By&nbsp;
-          <Typography component={Link} variant="subtitle2" href="https://codedthemes.com" target="_blank" underline="hover">
-            CodedThemes
-          </Typography>
+          <Typography variant="subtitle2" color="primary" component="span">
+            &copy;&nbsp;Cloudberry
+          </Typography>&nbsp;Faculty of Electrical Engineering, University of East Sarajevo
         </Typography>
 
         <Stack direction={matchDownSM ? 'column' : 'row'} spacing={matchDownSM ? 1 : 3} textAlign={matchDownSM ? 'center' : 'inherit'}>
@@ -26,31 +24,11 @@ const AuthFooter = () => {
             variant="subtitle2"
             color="secondary"
             component={Link}
-            href="https://material-ui.com/store/contributors/codedthemes/"
+            href="https://github.com/vascabarkapa/raspberry-cluster-analysis/"
             target="_blank"
             underline="hover"
           >
-            MUI Templates
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://codedthemes.com"
-            target="_blank"
-            underline="hover"
-          >
-            Privacy Policy
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            color="secondary"
-            component={Link}
-            href="https://codedthemes.support-hub.io/"
-            target="_blank"
-            underline="hover"
-          >
-            Support
+            GitHub
           </Typography>
         </Stack>
       </Stack>
