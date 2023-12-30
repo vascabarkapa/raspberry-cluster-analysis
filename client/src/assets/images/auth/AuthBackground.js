9d58e792451679294c1f50dfbba1eb4685e25dc6
vascabarkapa <vascabarkapa@gmail.com>
2023-12-30 22:05:07 +0100

    added favicon

--- client/src/assets/images/auth/AuthBackground.js
+++ client/src/assets/images/auth/AuthBackground.js
@@ -9,7 +9,7 @@ import cloudberry_logo from 'assets/images/logo/cloudberry_logo.png';
 const AuthBackground = () => {
   return (
     <Box sx={{ position: 'absolute', filter: 'blur(18px)', zIndex: -1, bottom: 0 }}>
-      <img src={cloudberry_logo} width="900rem" alt="cloudberry_logo" style={{marginLeft: '-100px'}} />
+      <img src={cloudberry_logo} width="900rem" alt="cloudberry_logo" style={{ marginLeft: '-100px' }} />
     </Box>
   );
 };
