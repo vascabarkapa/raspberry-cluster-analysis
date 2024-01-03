d7c99b82d17f0b4f3346108e0cd07f21a661a5ec
vascabarkapa <vascabarkapa@gmail.com>
2024-01-04 00:53:38 +0100

    add user button

--- client/src/pages/users/UsersPage.js
+++ client/src/pages/users/UsersPage.js
@@ -1,11 +1,14 @@
 // material-ui
-import { Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import { updatePageTitle } from '../../utils/TitleUtils';
 
 // project import
 import MainCard from 'components/MainCard';
 import UsersTable from './UsersTable';
 import { useEffect } from 'react';
+
+// assets
+import { UserAddOutlined } from '@ant-design/icons';
 
 // ==============================|| USERS PAGE ||============================== //
 
@@ -17,8 +20,16 @@ const UsersPage = () => {
   return (
     <Grid item xs={12} sx={{ mx: 5, my: 2 }}>
       <Grid container alignItems="center" justifyContent="space-between">
-        <Grid item>
-          <Typography variant="h3">Users</Typography>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h3">Users</Typography>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary">
+              <UserAddOutlined />
+              <Typography sx={{ ml: 1 }}>Add User</Typography>
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
       <MainCard sx={{ mt: 2 }} content={false}>
