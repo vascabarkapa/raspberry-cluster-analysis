e7241fea9c0d36d27e41a477262726e9a5a72697
vascabarkapa <vascabarkapa@gmail.com>
2024-01-04 02:01:58 +0100

    added delete modal

---
+++ client/src/pages/users/UsersDeleteModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+
+import { Typography } from '@mui/material';
+
+// eslint-disable-next-line
+const UsersDeleteModal = ({ open, setOpen }) => {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth>
+      <DialogTitle>
+        <Typography variant="h3">Delete User</Typography>
+      </DialogTitle>
+      <DialogContent>
+        <Box>
+          <Typography>Are you sure you want to delete the selected User?</Typography>
+          <Box mt={2} sx={{ textAlign: 'right' }}>
+            <Button type="button" variant="outlined" color="primary" sx={{ mr: 1 }} onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button type="submit" variant="contained" color="error" onClick={handleSubmit}>
+              Delete
+            </Button>
+          </Box>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UsersDeleteModal;
