31109fff1c3f5f23d14a2ef173edecb8c6125264
vascabarkapa <vascabarkapa@gmail.com>
2024-01-15 11:15:10 +0100

    constants for response statues codes

---
+++ server/shared/constants.js
@@ -0,0 +1,13 @@
+const responseStatusCodes = {
+  VALIDATION_ERROR: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500
+}
+
+const Constants = {
+  responseStatusCodes
+}
+
+export default Constants;
