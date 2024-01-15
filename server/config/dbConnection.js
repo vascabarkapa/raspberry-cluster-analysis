59450dfd2573b5c40d1a4e7c7b0ce2833a17429e
vascabarkapa <vascabarkapa@gmail.com>
2024-01-15 11:42:55 +0100

    db name

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+const {ConnectOptions} = require("mongoose");
+
+const dbOptions = {
+  dbName: 'cloudberry_local'
+}
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING, dbOptions);
     console.log("Database connected:", connect.connection.host, connect.connection.name);
   } catch (err) {
     console.error("Error connecting to database:", err.message);
