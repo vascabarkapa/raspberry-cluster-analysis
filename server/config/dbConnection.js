2ae606c04a90f2ed58e75e8ad9a1f92792d945db
vascabarkapa <vascabarkapa@gmail.com>
2024-01-15 11:34:59 +0100

    optimization of connection

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log("Database connected: " + connect.connection.host, connect.connection.name);
+    console.log("Database connected:", connect.connection.host, connect.connection.name);
   } catch (err) {
-    console.log(err);
+    console.error("Error connecting to database:", err.message);
     process.exit(1);
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-console.log('server works');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+
+connectDb();
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(cors({ origin: process.env.FRONT_URL }));
+server.use(express.json());
+server.use(errorHandler);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
