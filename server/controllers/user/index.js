758ccf488e5bef511eae405f1699ff21642fed71
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-01-16 21:49:24 +0100

    index user

---
+++ server/controllers/user/index.js
@@ -0,0 +1,5 @@
+export { default as getUsers } from './get-users';
+export { default as getUserById } from './get-user';
+export { default as createUser } from './create-user';
+export { default as updateUser } from './update-user';
+export { default as deleteUser } from './delete-user';
