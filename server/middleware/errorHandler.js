b77bece02c06d47040eb00376ee16e296dc3c493
vascabarkapa <vascabarkapa@gmail.com>
2024-01-17 09:41:23 +0100

    error handler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,34 +1,34 @@
-const Constants = require("../shared/constants");
+import Constants from './../shared/constants.js';
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
   switch (statusCode) {
     case Constants.responseStatusCodes.VALIDATION_ERROR:
-      res.json({title: "Validation failed!", message: err.message, stackTrace: err.stack});
+      res.json({title: Constants.responseStatusMessages.VALIDATION_ERROR, message: err.message, stackTrace: err.stack});
       break;
 
     case Constants.responseStatusCodes.UNAUTHORIZED:
-      res.json({title: "Unauthorized!", message: err.message, stackTrace: err.stack});
+      res.json({title: Constants.responseStatusMessages.UNAUTHORIZED, message: err.message, stackTrace: err.stack});
       break;
 
     case Constants.responseStatusCodes.FORBIDDEN:
-      res.json({title: "Forbidden!", message: err.message, stackTrace: err.stack});
+      res.json({title: Constants.responseStatusMessages.FORBIDDEN, message: err.message, stackTrace: err.stack});
       break;
 
     case Constants.responseStatusCodes.NOT_FOUND:
-      res.json({title: "Not found!", message: err.message, stackTrace: err.stack});
+      res.json({title: Constants.responseStatusMessages.NOT_FOUND, message: err.message, stackTrace: err.stack});
       break;
 
     case Constants.responseStatusCodes.SERVER_ERROR:
-      res.json({title: "Server error!", message: err.message, stackTrace: err.stack});
+      res.json({title: Constants.responseStatusMessages.SERVER_ERROR, message: err.message, stackTrace: err.stack});
       break;
 
     default:
-      console.log("No error!");
+      console.log(Constants.responseStatusMessages.DEFAULT);
       break;
   }
 
 };
 
-module.exports = errorHandler;
+export default errorHandler;
