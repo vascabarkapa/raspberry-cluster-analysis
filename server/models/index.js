ee667d22ad471cc0e7ff0432326ee288ff9cbc6a
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-01-17 19:32:59 +0100

    export models like index

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+import userModel from './user.js';
+import clusterModel from './cluster.js';
+import imageModel from './image.js';
+
+export const User = userModel;
+export const Cluster = clusterModel;
+export const Image = imageModel;
