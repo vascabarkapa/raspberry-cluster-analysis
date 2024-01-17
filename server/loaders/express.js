6ec5d3dedadcf0c2ed9f7cc3b69ca20b1b0ea39f
vascabarkapa <vascabarkapa@gmail.com>
2024-01-17 11:43:15 +0100

    libraries for better performance

--- server/loaders/express.js
+++ server/loaders/express.js
@@ -3,10 +3,23 @@ import cors from 'cors';
 import {prefix, frontUrl} from './../config/index.js';
 import routes from './../routes/index.js';
 import errorHandler from './../middleware/errorHandler.js';
+import compression from 'compression';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
 
 export default (server) => {
+  server.enable('trust proxy');
   server.use(cors({origin: frontUrl}));
+  server.use(bodyParser.urlencoded({ extended: false }));
+  server.use(bodyParser.json());
+  server.use(morgan('dev'));
+  server.use(helmet());
+  server.use(compression());
   server.use(express.static('public'));
+  server.disable('x-powered-by');
+  server.disable('etag');
+
   server.use(prefix, routes);
   server.use(errorHandler);
 
