15f8a9a8fc9eef6f78fc017cbbeda5641d3ca572
vascabarkapa <vascabarkapa@gmail.com>
2024-01-17 11:20:14 +0100

    prefix

--- server/loaders/express.js
+++ server/loaders/express.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './../routes/index.js';
 import errorHandler from './../middleware/errorHandler.js';
 
 export default (server) => {
@@ -7,5 +8,10 @@ export default (server) => {
 
   server.use(cors({origin: frontUrl}));
   server.use(express.static('public'));
+  server.use(process.env.PREFIX, routes);
   server.use(errorHandler);
+
+  server.get('/', (_req, res) => {
+    return res.status(200).json('Project is successfully working...').end();
+  });
 };
