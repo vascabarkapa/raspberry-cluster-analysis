f7673b1576268cb18b77a83223f120344cdb8f75
vascabarkapa <vascabarkapa@gmail.com>
2024-01-17 09:41:30 +0100

    responseStatusMessages

--- server/shared/constants.js
+++ server/shared/constants.js
@@ -6,8 +6,18 @@ const responseStatusCodes = {
   SERVER_ERROR: 500
 };
 
+const responseStatusMessages = {
+  VALIDATION_ERROR: 'Validation failed',
+  UNAUTHORIZED: 'Unauthorized',
+  FORBIDDEN: 'Forbidden',
+  NOT_FOUND: 'Not found',
+  SERVER_ERROR: 'Server error',
+  DEFAULT: 'No error'
+};
+
 const Constants = {
-  responseStatusCodes
+  responseStatusCodes,
+  responseStatusMessages
 };
 
 export default Constants;
