580f30d9561d0092c9c1e9cde6e10b27b95f0be0
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-01-18 22:58:53 +0100

    loading dots

---
+++ client/src/components/loading/Dots.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../../App.css';
+
+const Dots = () => {
+  return (
+    <div className="loading-dots">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
+
+export default Dots;
