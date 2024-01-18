1bc1cb295bc02b46b99434ecd178c34327db8e5c
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2024-01-18 22:34:16 +0100

    full logo

---
+++ client/src/components/logo/FullLogo.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+// assets
+import cloudberry from 'assets/images/logo/cloudberry.png';
+
+const CloudberryFullLogo = ({width}) => {
+    return (
+        <img src={cloudberry} width={width} alt="cloudberry_logo" />
+    )
+}
+
+CloudberryFullLogo.propTypes = {
+    width: PropTypes.number
+  };
+
+export default CloudberryFullLogo;
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -14,21 +15,45 @@ import 'assets/third-party/apex-chart.css';
 // project import
 import App from './App';
 import { store } from 'store';
+import CloudberryFullLogo from 'components/logo/FullLogo';
 import reportWebVitals from './reportWebVitals';
 
 // ==============================|| MAIN - REACT DOM RENDER  ||============================== //
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-  <StrictMode>
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
-  </StrictMode>
-);
+
+const MainApp = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 15000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <StrictMode>
+      <ReduxProvider store={store}>
+        <BrowserRouter>
+          {loading ? (
+            // Loading screen
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+              <CloudberryFullLogo width={300}/>
+            </div>
+          ) : (
+            // Main application
+            <App />
+          )}
+        </BrowserRouter>
+      </ReduxProvider>
+    </StrictMode>
+  );
+};
+
+root.render(<MainApp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
