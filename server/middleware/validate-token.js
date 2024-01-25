89b10a3f0186753ce661f99272bc462595a80fec
vascabarkapa <vascabarkapa@gmail.com>
2024-01-25 23:39:27 +0100

    fixed validation

--- server/middleware/validate-token.js
+++ server/middleware/validate-token.js
@@ -1,4 +1,4 @@
-import User from '../models/user.js';
+import {User} from '../models/index.js';
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import {accessTokenSecret} from './../config/index.js';
