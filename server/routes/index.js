3bbd78997b1bd4565ff64c6db286337014814a24
vascabarkapa <vascabarkapa@gmail.com>
2024-01-25 23:01:05 +0100

    fixed routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,12 @@ import {Router} from 'express';
 import userRoutes from './userRoutes.js';
 import clusterRoutes from './clusterRoutes.js';
 import authRoutes from './authRoutes.js';
+import imageRoutes from './imageRoutes.js';
 
 const router = Router();
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/cluster', clusterRoutes);
-router.use('/image', clusterRoutes);
+router.use('/image', imageRoutes);
 
 export default router;
