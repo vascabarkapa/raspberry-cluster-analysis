24de4ff79481c09112d21877d737a33bff0b3621
vascabarkapa <vascabarkapa@gmail.com>
2024-01-25 17:30:31 +0100

    mongoose fix

--- server/loaders/mongoose.js
+++ server/loaders/mongoose.js
@@ -1,16 +1,12 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import {environment, connectionString} from './../config/index.js';
+import {connectionString} from './../config/index.js';
 
 dotenv.config();
 
-const dbOptions = {
-  dbName: environment
-}
-
 export default async () => {
   try {
-    const connect = await mongoose.connect(connectionString, dbOptions);
+    const connect = await mongoose.connect(connectionString);
     console.log("Database connected:", connect.connection.host, connect.connection.name);
   } catch (err) {
     console.error("Error connecting to database:", err.message);
