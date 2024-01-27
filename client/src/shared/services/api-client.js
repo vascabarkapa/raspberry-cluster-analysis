4c0abf417b6d61834d1148e35695254b5639b3da
vascabarkapa <vascabarkapa@gmail.com>
2024-01-28 00:18:53 +0100

    refactored api client

--- client/src/shared/services/api-client.js
+++ client/src/shared/services/api-client.js
@@ -1,19 +1,9 @@
 import axiosInstance from './axios';
 
-const get = (endpoint, query = '') => {
-  return axiosInstance.get(`${endpoint}?${query}`);
-};
-
-const post = (endpoint, body, query = '', config = {}) => {
-  return axiosInstance.post(`${endpoint}?${query}`, body, config);
-};
-
-const put = (endpoint, body, query = '') => {
-  return axiosInstance.put(`${endpoint}?${query}`, body);
-};
-
-const remove = (endpoint, query = '') => {
-  return axiosInstance.delete(`${endpoint}?${query}`);
-};
+const get = (endpoint, params = {}) => axiosInstance.get(endpoint, { params });
+const post = (endpoint, body, params = {}, config = {}) => axiosInstance.post(endpoint, body, { params, ...config });
+const put = (endpoint, body, params = {}) => axiosInstance.put(endpoint, body, { params });
+const remove = (endpoint, params = {}) => axiosInstance.delete(endpoint, { params });
 
 export { get, post, put, remove };
+
