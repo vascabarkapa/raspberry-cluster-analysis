80ee4b0cb81cbfb20730f3a775eb36f83ce091f4
vascabarkapa <vascabarkapa@gmail.com>
2024-01-28 00:04:06 +0100

    change loading

--- client/src/index.js
+++ client/src/index.js
@@ -37,7 +37,7 @@ const MainApp = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1500);
+    }, 700);
 
     return () => clearTimeout(timer);
   }, []);
