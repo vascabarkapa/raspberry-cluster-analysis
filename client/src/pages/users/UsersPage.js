dae4f485b874ab0a7dd19f30e3a34cfbf93117c9
vascabarkapa <vascabarkapa@gmail.com>
2024-01-27 19:14:25 +0100

    refactored user page

--- client/src/pages/users/UsersPage.js
+++ client/src/pages/users/UsersPage.js
@@ -1,53 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 // material-ui
-import { Button, Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import { updatePageTitle } from '../../utils/TitleUtils';
 
 // project import
-import MainCard from 'components/MainCard';
 import UsersTable from './UsersTable';
-
-// assets
-import { UserAddOutlined } from '@ant-design/icons';
-
-// components
-import UsersFormModal from './UsersFormModal';
 
 // ==============================|| USERS PAGE ||============================== //
 
 const UsersPage = () => {
-  const [openUsersFormModal, setOpenUsersFormModal] = useState(false);
-
   useEffect(() => {
     updatePageTitle('Users - Cloudberry');
   }, []);
 
-  const handleOpen = () => {
-    setOpenUsersFormModal(true);
-  };
 
   return (
     <>
       <Grid item xs={12} sx={{ mx: { xs: 1, md: 5 }, my: { xs: 1, md: 2 } }}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h3">Users</Typography>
-            </Grid>
-            <Grid item>
-              <Button variant="contained" color="primary" onClick={handleOpen}>
-                <UserAddOutlined />
-                <Typography sx={{ ml: 1 }}>Add User</Typography>
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-        <MainCard sx={{ mt: 2 }} content={false}>
-          <UsersTable />
-        </MainCard>
+        <UsersTable />
       </Grid>
-      {openUsersFormModal && <UsersFormModal open={openUsersFormModal} setOpen={setOpenUsersFormModal} />}
     </>
   );
 };
--- client/src/pages/users/UsersTable.js
+++ client/src/pages/users/UsersTable.js
@@ -1,10 +1,21 @@
 import { useEffect, useState } from 'react';
 
 // material-ui
-import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tooltip, Typography
+} from '@mui/material';
 
 // assets
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined, UserAddOutlined } from '@ant-design/icons';
 
 // services
 import UserService from '../../shared/services/user-service';
@@ -14,6 +25,7 @@ import UsersFormModal from './UsersFormModal';
 import UsersDeleteModal from './UsersDeleteModal';
 import TableLoading from '../../components/loading/TableLoading';
 import TableEmpty from '../../components/loading/TableEmpty';
+import MainCard from '../../components/MainCard';
 
 // ==============================|| USERS TABLE - HEADER CELL ||============================== //
 
@@ -73,7 +85,7 @@ export default function UsersTable() {
   const [trigger, setTrigger] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [users, setUsers] = useState([]);
-  // const [userToEdit, setUserToEdit] = useState(null);
+  const [userToForm, setUserToForm] = useState(null);
   const [userToDelete, setUserToDelete] = useState(null);
   const [openUsersFormModal, setOpenUsersFormModal] = useState(false);
   const [openUsersDeleteModal, setOpenDeleteFormModal] = useState(false);
@@ -86,6 +98,7 @@ export default function UsersTable() {
         if (response) {
           setUsers(response?.data);
           setIsLoading(false);
+          setTrigger(false);
         }
       });
     }
@@ -93,9 +106,15 @@ export default function UsersTable() {
     setInitialRender(false);
   }, [initialRender, trigger]);
 
-  const handleOpenUsersFormModal = () => {
+  function handleOpenUsersFormModal(type = 'add', user = null) {
+    setUserToForm(null);
+
+    if (type === 'edit') {
+      setUserToForm(user);
+    }
+
     setOpenUsersFormModal(true);
-  };
+  }
 
   function handleOpenUsersDeleteModal(user) {
     setUserToDelete(user);
@@ -103,69 +122,90 @@ export default function UsersTable() {
   }
 
   return (
-    <Box>
-      <TableContainer
-        sx={{
-          width: '100%',
-          overflowX: 'auto',
-          position: 'relative',
-          display: 'block',
-          maxWidth: '100%',
-          '& td, & th': { whiteSpace: 'nowrap' }
-        }}
-      >
-        <Table
-          aria-labelledby="tableTitle"
-          sx={{
-            '& .MuiTableCell-root:first-of-type': {
-              pl: 2
-            },
-            '& .MuiTableCell-root:last-of-type': {
-              pr: 3
-            }
-          }}
-        >
-          <UsersTableHead />
-          {isLoading ? (
-            <TableLoading colSpan={8} />
-          ) : (
-            <TableBody>
-              {users && users?.length > 0 ? (
-                users.map((user) => {
-                  return (
-                    <TableRow hover sx={{ '&:last-child td, &:last-child th': { border: 0 } }} key={user._id}>
-                      <TableCell align="left">
-                        {user.first_name}&nbsp;{user.last_name}
-                      </TableCell>
-                      <TableCell align="left">{user.username}</TableCell>
-                      <TableCell align="left">{user.email}</TableCell>
-                      <TableCell align="left">{user.role}</TableCell>
-                      <TableCell align="right">
-                        <Tooltip title="Edit User">
-                          <Button variant="contained" color="primary" sx={{ mr: 2 }} onClick={handleOpenUsersFormModal}>
-                            <EditOutlined />
-                          </Button>
-                        </Tooltip>
-                        <Tooltip title="Delete User">
-                          <Button variant="contained" color="error" onClick={() => handleOpenUsersDeleteModal(user)}>
-                            <DeleteOutlined />
-                          </Button>
-                        </Tooltip>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })
+    <>
+      <Grid container alignItems="center" justifyContent="space-between">
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h3">Users</Typography>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={handleOpenUsersFormModal}>
+              <UserAddOutlined />
+              <Typography sx={{ ml: 1 }}>Add User</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <MainCard sx={{ mt: 2 }} content={false}>
+        <Box>
+          <TableContainer
+            sx={{
+              width: '100%',
+              overflowX: 'auto',
+              position: 'relative',
+              display: 'block',
+              maxWidth: '100%',
+              '& td, & th': { whiteSpace: 'nowrap' }
+            }}
+          >
+            <Table
+              aria-labelledby="tableTitle"
+              sx={{
+                '& .MuiTableCell-root:first-of-type': {
+                  pl: 2
+                },
+                '& .MuiTableCell-root:last-of-type': {
+                  pr: 3
+                }
+              }}
+            >
+              <UsersTableHead />
+              {isLoading ? (
+                <TableLoading colSpan={8} />
               ) : (
-                <TableEmpty colSpan={8} text={'No Image information'} />
+                <TableBody>
+                  {users && users?.length > 0 ? (
+                    users.map((user) => {
+                      return (
+                        <TableRow hover sx={{ '&:last-child td, &:last-child th': { border: 0 } }} key={user._id}>
+                          <TableCell align="left">
+                            {user.first_name}&nbsp;{user.last_name}
+                          </TableCell>
+                          <TableCell align="left">{user.username}</TableCell>
+                          <TableCell align="left">{user.email}</TableCell>
+                          <TableCell align="left">{user.role}</TableCell>
+                          <TableCell align="right">
+                            <Tooltip title="Edit User">
+                              <Button variant="contained" color="primary" sx={{ mr: 2 }}
+                                      onClick={() => handleOpenUsersFormModal('edit', user)}>
+                                <EditOutlined />
+                              </Button>
+                            </Tooltip>
+                            <Tooltip title="Delete User">
+                              <Button variant="contained" color="error"
+                                      onClick={() => handleOpenUsersDeleteModal(user)}>
+                                <DeleteOutlined />
+                              </Button>
+                            </Tooltip>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })
+                  ) : (
+                    <TableEmpty colSpan={8} text={'No Image information'} />
+                  )}
+                </TableBody>
               )}
-            </TableBody>
-          )}
-        </Table>
-      </TableContainer>
-      {openUsersFormModal && <UsersFormModal open={openUsersFormModal} setOpen={setOpenUsersFormModal} />}
-      {openUsersDeleteModal &&
-        <UsersDeleteModal open={openUsersDeleteModal} setOpen={setOpenDeleteFormModal} user={userToDelete}
-                          setTrigger={setTrigger} />}
-    </Box>
+            </Table>
+          </TableContainer>
+          {openUsersFormModal &&
+            <UsersFormModal open={openUsersFormModal} setOpen={setOpenUsersFormModal} user={userToForm}
+                            setTrigger={setTrigger} />}
+          {openUsersDeleteModal &&
+            <UsersDeleteModal open={openUsersDeleteModal} setOpen={setOpenDeleteFormModal} user={userToDelete}
+                              setTrigger={setTrigger} />}
+        </Box>
+      </MainCard>
+    </>
   );
 }
