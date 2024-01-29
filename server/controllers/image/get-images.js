5ba694766e88988714911d6fd8373cb226e855d1
vascabarkapa <vascabarkapa@gmail.com>
2024-01-29 10:59:52 +0100

    get image pagination

--- server/controllers/image/get-images.js
+++ server/controllers/image/get-images.js
@@ -3,10 +3,26 @@ import { Image } from './../../models/index.js';
 
 export default asyncHandler(async (req, res) => {
   try {
-    const images = await Image.find().sort({createdAt: -1});
-    res.json(images);
+    const page = parseInt(req.query.page, 10) || 1;
+    const perPage = 10;
+
+    const skip = (page - 1) * perPage;
+
+    const images = await Image.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(perPage);
+
+    const totalImages = await Image.countDocuments();
+    const totalPages = Math.ceil(totalImages / perPage);
+
+    res.json({
+      images: images,
+      total_pages: totalPages,
+      current_page: page
+    });
   } catch (error) {
-    console.error("Error retrieving images from the database:", error);
-    res.status(500).json({message: "Internal Server Error"});
+    console.error('Error retrieving images from the database:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
