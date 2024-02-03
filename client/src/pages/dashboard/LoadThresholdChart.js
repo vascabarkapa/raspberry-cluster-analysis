03eeed9bdd1b2e327b0477d6f71145a56d6f2cd6
vascabarkapa <vascabarkapa@gmail.com>
2024-02-04 00:01:05 +0100

    dummy load threshold chart

---
+++ client/src/pages/dashboard/LoadThresholdChart.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+
+// third-party
+import ReactApexChart from 'react-apexcharts';
+
+// chart options
+const areaChartOptions = {
+  chart: {
+    height: 450,
+    type: 'area',
+    toolbar: {
+      show: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth',
+    width: 2
+  },
+  grid: {
+    strokeDashArray: 0
+  }
+};
+
+// ==============================|| INCOME AREA CHART ||============================== //
+
+const LoadThresholdChart = () => {
+  const theme = useTheme();
+
+  const { primary, secondary } = theme.palette.text;
+  const line = theme.palette.divider;
+
+  const [options, setOptions] = useState(areaChartOptions);
+
+  useEffect(() => {
+    setOptions((prevState) => ({
+      ...prevState,
+      colors: [theme.palette.primary.main, theme.palette.primary[700]],
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        labels: {
+          style: {
+            colors: [
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary,
+              secondary
+            ]
+          }
+        },
+        axisBorder: {
+          show: true,
+          color: line
+        },
+        tickAmount: 11
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: [secondary]
+          }
+        }
+      },
+      grid: {
+        borderColor: line
+      },
+      tooltip: {
+        theme: 'light'
+      }
+    }));
+  }, [primary, secondary, line, theme]);
+
+  const [series, setSeries] = useState([
+    {
+      name: 'Page Views',
+      data: [0, 86, 28, 115, 48, 210, 136]
+    },
+    {
+      name: 'Sessions',
+      data: [0, 43, 14, 56, 24, 105, 68]
+    }
+  ]);
+
+  useEffect(() => {
+    setSeries([
+      {
+        name: 'Page Views',
+        data: [76, 85, 101, 98, 87, 105, 91, 114, 94, 86, 115, 35]
+      },
+      {
+        name: 'Sessions',
+        data: [110, 60, 150, 35, 60, 36, 26, 45, 65, 52, 53, 41]
+      }
+    ]);
+  }, []);
+
+  return <ReactApexChart options={options} series={series} type="area" height={450} />;
+};
+
+export default LoadThresholdChart;
--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -7,6 +7,7 @@ import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 // project import
 import IncomeAreaChart from './IncomeAreaChart';
 import MonthlyBarChart from './MonthlyBarChart';
+import LoadThresholdChart from './LoadThresholdChart';
 import MainCard from 'components/MainCard';
 
 // toast
@@ -30,60 +31,18 @@ const DashboardDefault = () => {
 
   return (
     <Grid item xs={12} sx={{ mx: { xs: 1, md: 5 }, my: { xs: 1, md: 2 } }}>
-      <Typography sx={{ marginBottom: '2rem' }}>
-        <b>NOTE: This is a test version. Project under development.</b>
-      </Typography>
       <Grid container rowSpacing={4.5} columnSpacing={2.75}>
         {/* row 1 */}
-        <Grid item xs={12} md={7} lg={8}>
+        <Grid item xs={12} md={12} lg={12}>
           <Grid container alignItems="center" justifyContent="space-between">
             <Grid item>
-              <Typography variant="h5">Cluster Details</Typography>
-            </Grid>
-            <Grid item>
-              <Stack direction="row" alignItems="center" spacing={0}>
-                <Button
-                  size="small"
-                  onClick={() => setSlot('month')}
-                  color={slot === 'month' ? 'primary' : 'secondary'}
-                  variant={slot === 'month' ? 'outlined' : 'text'}
-                >
-                  Month
-                </Button>
-                <Button
-                  size="small"
-                  onClick={() => setSlot('week')}
-                  color={slot === 'week' ? 'primary' : 'secondary'}
-                  variant={slot === 'week' ? 'outlined' : 'text'}
-                >
-                  Week
-                </Button>
-              </Stack>
+              <Typography variant="h5">Cluster Load in the last 24 hours</Typography>
             </Grid>
           </Grid>
           <MainCard content={false} sx={{ mt: 1.5 }}>
             <Box sx={{ pt: 1, pr: 2 }}>
-              <IncomeAreaChart slot={slot} />
+              <LoadThresholdChart slot={slot} />
             </Box>
-          </MainCard>
-        </Grid>
-        <Grid item xs={12} md={5} lg={4}>
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h5">Weekly Average</Typography>
-            </Grid>
-            <Grid item />
-          </Grid>
-          <MainCard sx={{ mt: 2 }} content={false}>
-            <Box sx={{ p: 3, pb: 0 }}>
-              <Stack spacing={2}>
-                <Typography variant="h6" color="textSecondary">
-                  This Week Statistics
-                </Typography>
-                <Typography variant="h3">50</Typography>
-              </Stack>
-            </Box>
-            <MonthlyBarChart />
           </MainCard>
         </Grid>
 
