e48470a4ba2c8b886a6d08481f77706cb348ca9a
vascabarkapa <vascabarkapa@gmail.com>
2024-02-04 19:16:02 +0100

    get greeting message

---
+++ client/src/utils/GreetingMessage.js
@@ -0,0 +1,16 @@
+export const getGreetingMessage = () => {
+  const CURRENT_USER = JSON.parse(localStorage.getItem('current_user'));
+
+  const currentHour = new Date().getHours();
+
+  let greetingMessage;
+  if (currentHour >= 4 && currentHour < 12) {
+    greetingMessage = "Good morning";
+  } else if (currentHour >= 12 && currentHour < 20) {
+    greetingMessage = "Good afternoon";
+  } else {
+    greetingMessage = "Good evening";
+  }
+
+  return greetingMessage + ", " + CURRENT_USER?.first_name + " " + CURRENT_USER?.last_name;
+};
