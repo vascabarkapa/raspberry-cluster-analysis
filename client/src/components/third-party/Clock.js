84ccbdcf7fcf49a01c5ecd0d1a6ed0293d88437f
vascabarkapa <vascabarkapa@gmail.com>
2024-02-04 19:15:56 +0100

    clock

---
+++ client/src/components/third-party/Clock.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatTime = (time) => {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const formattedHours = (hours < 10) ? '0' + hours : hours;
+    const formattedMinutes = (minutes < 10) ? '0' + minutes : minutes;
+    const formattedSeconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  return formatTime(currentTime);
+};
+
+export default Clock;
