755892ecc214915da95d43419fda38a576d61963
vascabarkapa <vascabarkapa@gmail.com>
2024-02-04 23:03:03 +0100

    get notifications

---
+++ server/controllers/notification/get-notifications.js
@@ -0,0 +1,17 @@
+import asyncHandler from 'express-async-handler';
+import { Notification } from './../../models/index.js';
+
+export default asyncHandler(async (req, res) => {
+  try {
+    const { user_id } = req.params;
+
+    const unreadNotifications = await Notification.find({ user_id, is_read: false }).sort({ createdAt: -1 });
+    const readNotifications = await Notification.find({ user_id, is_read: true }).sort({ createdAt: -1 });
+    const allNotifications = [...unreadNotifications, ...readNotifications];
+
+    res.json(allNotifications);
+  } catch (error) {
+    console.error('Error retrieving notifications from the database:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
