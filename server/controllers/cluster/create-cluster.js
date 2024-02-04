228836ecc02beeb9c76c2233233ca63655e16f6c
vascabarkapa <vascabarkapa@gmail.com>
2024-02-05 00:53:02 +0100

    create notification for all admin users

--- server/controllers/cluster/create-cluster.js
+++ server/controllers/cluster/create-cluster.js
@@ -1,21 +1,23 @@
 import asyncHandler from 'express-async-handler';
-import { Cluster, Notification } from './../../models/index.js';
+import { Cluster, Notification, User } from './../../models/index.js';
 
 export default asyncHandler(async (req, res) => {
   try {
     const newCluster = await Cluster.create(req.body);
 
     if (newCluster.replicas > newCluster.max_pods) {
-      await Notification.create(
-        {
-          user_id: '65bfe8b94db8d70019f07b81',
+      const adminUsers = await User.find({ role: 'admin' });
+
+      adminUsers.forEach(async (adminUser) => {
+        await Notification.create({
+          user_id: adminUser._id,
           model_name: 'Cluster',
           model_id: newCluster._id,
           title: 'Your cluster is overloaded!',
           description: 'Check the health of the cluster.',
           is_read: false
-        }
-      );
+        });
+      });
     }
 
     res.status(201).json(newCluster);
