6f71516eabfbfef2aa5165d78d4e88eae948a6b4
vascabarkapa <vascabarkapa@gmail.com>
2024-02-05 00:23:03 +0100

    read all  route

--- server/controllers/notification/index.js
+++ server/controllers/notification/index.js
@@ -1,2 +1,3 @@
 export { default as getNotifications } from './get-notifications.js';
 export { default as readNotification } from './read-notification.js';
+export { default as readAllNotifications } from './read-all-notifications.js';
--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getNotifications, readNotification } from '../controllers/notification/index.js';
+import { getNotifications, readNotification, readAllNotifications } from '../controllers/notification/index.js';
 import { auth } from '../middleware/index.js';
 
 const router = Router();
 
 router.get('/:id', auth, getNotifications);
 router.post('/:id', auth, readNotification);
+router.post('/read-all/:id', auth, readAllNotifications);
 
 export default router;
