cf03b01371404e27ae12bb87b42977bbc6b6dad1
vascabarkapa <vascabarkapa@gmail.com>
2024-02-04 23:20:36 +0100

    notification service

---
+++ client/src/shared/services/notification-service.js
@@ -0,0 +1,19 @@
+import { get, post } from './api-client';
+
+const ENDPOINT = '/notification';
+const CURRENT_USER = JSON.parse(localStorage.getItem('current_user'));
+
+function getNotifications() {
+  return get(ENDPOINT + '/' + CURRENT_USER?._id);
+}
+
+function readNotification(id) {
+  return post(ENDPOINT + '/' + id);
+}
+
+const NotificationService = {
+  getNotifications,
+  readNotification
+};
+
+export default NotificationService;
