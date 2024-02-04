a847bd0e8a502b3a8a7c488f7721d2963200dc1d
vascabarkapa <vascabarkapa@gmail.com>
2024-02-04 19:16:08 +0100

    get current date

---
+++ client/src/utils/CurrentDate.js
@@ -0,0 +1,8 @@
+export const getCurrentDate = () => {
+  const danas = new Date();
+  const dan = String(danas.getDate()).padStart(2, '0');
+  const mesec = String(danas.getMonth() + 1).padStart(2, '0');
+  const godina = danas.getFullYear();
+
+  return `${dan}.${mesec}.${godina}`;
+}
