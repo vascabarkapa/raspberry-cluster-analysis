f97cfbf8a6298ccdb0476450401867bbce0cf979
vascabarkapa <vascabarkapa@gmail.com>
2024-02-05 20:17:54 +0100

    diff function

--- server/shared/email-helper.js
+++ server/shared/email-helper.js
@@ -4,25 +4,7 @@ import { nodemailerUser, nodemailerPassword } from './../config/index.js';
 
 export default async function sendEmail(cluster, adminUser, subject, to) {
   try {
-    const htmlTemplate = fs.readFileSync('./shared/templates/mail-cluster-overload.html', 'utf8');
-    const emailContent = htmlTemplate
-      .replace('[Name]', adminUser?.first_name + ' ' + adminUser?.last_name)
-      .replace('[ID]', cluster?._id)
-      .replace('[Age]', cluster?.age)
-      .replace('[Nodes]', cluster?.number_of_nodes || 'N/A')
-      .replace('[Min Pods]', cluster?.min_pods)
-      .replace('[Max Pods]', cluster?.max_pods)
-      .replace('[Replicas]', cluster?.replicas)
-      .replace('[Load]', cluster?.load)
-      .replace('[Load Threshold]', cluster?.load_threshold)
-      .replace('[Date Time]', cluster?.createdAt.toLocaleString('sr-RS', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-      }));
+    const emailContent = createClusterOverloadEmailTemplate(cluster, adminUser);
 
     let transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -42,4 +24,26 @@ export default async function sendEmail(cluster, adminUser, subject, to) {
   } catch (err) {
     console.error('Error sending email:', err);
   }
+}
+
+function createClusterOverloadEmailTemplate(cluster, adminUser) {
+  const htmlTemplate = fs.readFileSync('./shared/templates/mail-cluster-overload.html', 'utf8');
+  return htmlTemplate
+    .replace('[Name]', adminUser?.first_name + ' ' + adminUser?.last_name)
+    .replace('[ID]', cluster?._id)
+    .replace('[Age]', cluster?.age)
+    .replace('[Nodes]', cluster?.number_of_nodes || 'N/A')
+    .replace('[Min Pods]', cluster?.min_pods)
+    .replace('[Max Pods]', cluster?.max_pods)
+    .replace('[Replicas]', cluster?.replicas)
+    .replace('[Load]', cluster?.load)
+    .replace('[Load Threshold]', cluster?.load_threshold)
+    .replace('[Date Time]', cluster?.createdAt.toLocaleString('sr-RS', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    }));
 }
