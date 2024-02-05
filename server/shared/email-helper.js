1e49177485c651a1fcf73e9f6ad092a322eb4ad2
vascabarkapa <vascabarkapa@gmail.com>
2024-02-05 20:15:35 +0100

    loaded data

--- server/shared/email-helper.js
+++ server/shared/email-helper.js
@@ -2,31 +2,27 @@ import fs from 'fs';
 import nodemailer from 'nodemailer';
 import { nodemailerUser, nodemailerPassword } from './../config/index.js';
 
-export default async function sendEmail(subject, text) {
+export default async function sendEmail(cluster, adminUser, subject, to) {
   try {
     const htmlTemplate = fs.readFileSync('./shared/templates/mail-cluster-overload.html', 'utf8');
-    const emailContent = htmlTemplate;
-      // .replace('[Name]', newTicket?.first_name + " " + newTicket?.last_name)
-      // .replace('[number of tickets]', newTicket?.number_of_tickets)
-      // .replace('[movie title]', repertoryToBuy?.movie?.name)
-      // .replace('[date]', repertoryToBuy?.dateTime.toLocaleString('sr-RS', {
-      //   day: '2-digit',
-      //   month: '2-digit',
-      //   year: 'numeric',
-      //   hour: '2-digit',
-      //   minute: '2-digit'
-      // }))
-      // .replace('[time]', repertoryToBuy?.dateTime)
-      // .replace('[order number]', repertoryToBuy?._id)
-      // .replace('[total]', newTicket?.sum_price)
-      // .replace('[date purchase]', newTicket?.createdAt.toLocaleString('sr-RS', {
-      //   day: '2-digit',
-      //   month: '2-digit',
-      //   year: 'numeric',
-      //   hour: '2-digit',
-      //   minute: '2-digit',
-      //   second: '2-digit'
-      // }));
+    const emailContent = htmlTemplate
+      .replace('[Name]', adminUser?.first_name + ' ' + adminUser?.last_name)
+      .replace('[ID]', cluster?._id)
+      .replace('[Age]', cluster?.age)
+      .replace('[Nodes]', cluster?.number_of_nodes || 'N/A')
+      .replace('[Min Pods]', cluster?.min_pods)
+      .replace('[Max Pods]', cluster?.max_pods)
+      .replace('[Replicas]', cluster?.replicas)
+      .replace('[Load]', cluster?.load)
+      .replace('[Load Threshold]', cluster?.load_threshold)
+      .replace('[Date Time]', cluster?.createdAt.toLocaleString('sr-RS', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      }));
 
     let transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -38,8 +34,8 @@ export default async function sendEmail(subject, text) {
     });
 
     return await transporter.sendMail({
-      from: 'Cloudberry Systems',
-      to: 'vascabarkapa@gmail.com',
+      from: 'Cloudberry',
+      to: to,
       subject: subject,
       html: emailContent
     });
